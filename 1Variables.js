5f586271470e50281a1d86e50e7ae527e3641d32
Pratiksha Bhabad <92775102+Pratikshabhabad@users.noreply.github.com>
2024-01-05 16:35:23 +0530

    Add files via upload

---
+++ 1Variables.js
@@ -0,0 +1,14 @@
+const accountId=234;
+
+let accountEmail="pratik@gmail.com";
+var accountPass="1234";
+accountCity="jaipur";
+
+
+accountCity="sal"
+console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPass);
+// console.log(accountCity);
+
+console.table([accountEmail,accountId,accountPass,accountCity])
