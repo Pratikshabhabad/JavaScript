71a7e6659976ea0cc98808ea872684d96e823d00
Pratiksha Bhabad <92775102+Pratikshabhabad@users.noreply.github.com>
2024-01-07 15:47:31 +0530

    Add files via upload

---
+++ 2dataType.js
@@ -0,0 +1,18 @@
+"use Strict" //code treated  as new code/version
+//alert(3+3)//we using node js,not browser
+
+
+//primitives-
+//number
+//bigint
+//String = ""
+//boolean= true/false
+//null
+//undefined 
+//symbol-unique
+
+
+//Objectives
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
---
+++ 3conversion.js
@@ -0,0 +1,30 @@
+//let score="33";
+//let score=null
+// let score="12ab"
+// console.log(typeof(score));
+
+// let value=Number(score)
+// console.log(typeof value);
+// console.log(value);
+
+// "33"=>33
+//"33abd"=>NAN
+//true=1 false=0
+
+// let islogin="hit"
+// let booleanisLen=Boolean(islogin);
+
+// let islogin=""
+// let booleanisLen=Boolean(islogin);
+
+let islogin=1
+let booleanisLen=Boolean(islogin);
+
+console.log(booleanisLen);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log((3+4)*5%3);
