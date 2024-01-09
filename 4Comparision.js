410f8864df38d4c920c0eb6629f6afc5afe973df
Pratiksha Bhabad <92775102+Pratikshabhabad@users.noreply.github.com>
2024-01-09 16:32:43 +0530

    Add files via upload

---
+++ 4Comparision.js
@@ -0,0 +1,9 @@
+console.log(null>0);//both are different (1,3)//result false
+console.log(null==0);//false
+console.log(null>=0);// true
+
+console.log(undefined>0);//false
+console.log(undefined==0);//false
+console.log(undefined<0);//false
+
+console.log(2==2);///are normal operations
---
+++ 5DataTypesArray.js
@@ -0,0 +1,19 @@
+const no=234638579492293n
+
+const arr=["apple","tulip","rose","guava"];
+console.log(arr);//obj
+console.log(typeof(arr));
+
+let myobj={
+    name:"hero",
+    age:22,
+}
+
+console.log(myobj);
+console.log(typeof(myobj));//obj
+
+const myfun=function(){
+    console.log("hellow world");
+}
+
+console.log(typeof(myfun));//function
