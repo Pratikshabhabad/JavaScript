a9701fbe4c7a83417e12fffd5c0a8787f191cc3d
Pratiksha Bhabad <92775102+Pratikshabhabad@users.noreply.github.com>
2024-01-14 11:20:40 +0530

    Add files via upload

---
+++ 14Objects_3.js
@@ -0,0 +1,28 @@
+const course={
+    courseName:"JS in HINDI",
+    coursePrice:"999",
+    courseInstructor:"Hitesh"
+}
+
+const {courseName}=course  //another type of getting value
+const{courseInstructor:instructor}=course //short naming
+console.log(courseName);
+console.log(instructor);//destructuring use in react 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
